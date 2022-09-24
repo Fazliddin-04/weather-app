9aa397f715c23074b9239db24b12a730dc45a90f
fazliddin <fazliddinpardayev004@gmail.com>
2022-09-24 12:11:11 +0500

    search results optimized

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { FiSearch } from 'react-icons/fi'
 import { useDispatch } from 'react-redux'
 import { getWeather } from '../features/weather/weatherSlice'
@@ -8,7 +8,8 @@ function SearchBar() {
   const [searchResults, setSearchResults] = useState([])
   const dispatch = useDispatch()
 
-  useEffect(() => {
+  const onChange = (e) => {
+    setSearchRegion(e)
     if (searchRegion) {
       fetch(
         `https://api.weatherapi.com/v1/search.json?key=3f26bfeb5b2349c692874026222109&q=${searchRegion}`
@@ -20,13 +21,12 @@ function SearchBar() {
           setSearchResults(payload)
         })
     }
-  }, [searchRegion])
+  }
 
   const onSubmit = (e) => {
     e.preventDefault()
 
     dispatch(getWeather(searchRegion))
-    setSearchRegion('')
   }
 
   const searchResultClick = (region) => {
@@ -41,7 +41,7 @@ function SearchBar() {
         type="text"
         placeholder="Another Location"
         value={searchRegion}
-        onChange={(e) => setSearchRegion(e.target.value)}
+        onChange={(e) => onChange(e.target.value)}
       />
       <button title="Search..." type="submit">
         <FiSearch />
